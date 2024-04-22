8cc526b46a6a2a18f09cc54af5189df1683809b9
[sigit-khoirun-nizam] <[sigitkhoirunnizam10@gmail.com]>
2024-04-22 09:54:05 +0700

    end

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "Satoshi", "cursive"]
+      },
+      colors: {
+        primary1: "#003E29",
+        primary2: "#E4BBA1",
+        primary3: "#D9D9D9",
+        borderColor: "#787878",
+        bgColor: "#EDEDED",
+        bgGreen: "#023423"
+      }
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
